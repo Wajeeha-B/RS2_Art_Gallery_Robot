eaa22993a29cda095b676d75716e8b625508f2ad
stmn8r <ali.sarlak@student.uts.edu.au>
2024-04-08 16:08:59 +1000

    Ali's implementation uncommented

--- artbot_code/src/laserprocessing.cpp
+++ artbot_code/src/laserprocessing.cpp
@@ -4,28 +4,28 @@
 
 using namespace std;
 
-//Default constructor of Laserprocessing, needs an initial set of laser data to be initialised
-LaserProcessing::LaserProcessing(sensor_msgs::LaserScan laserScan):
-    laserScan_(laserScan){
+// //Default constructor of Laserprocessing, needs an initial set of laser data to be initialised
+// LaserProcessing::LaserProcessing(sensor_msgs::LaserScan laserScan):
+//     laserScan_(laserScan){
         
-    }
+//     }
 
-//Getter for distance and angle to the nearest obstacle
-std::pair<double, double> LaserProcessing::MinDistAngle(){
-    std::pair<double, double> distAngle;
-    distAngle.first = 0.0;
-    distAngle.second = 1.0;
+// //Getter for distance and angle to the nearest obstacle
+// std::pair<double, double> LaserProcessing::MinDistAngle(){
+//     std::pair<double, double> distAngle;
+//     distAngle.first = 0.0;
+//     distAngle.second = 1.0;
 
-    return distAngle;
-}
+//     return distAngle;
+// }
 
 // Ali's code
 
 // Getter for distance and angle to the nearest obstacle
-// std::pair<double, double> LaserProcessing::MinDistAngle() {
-//     std::pair<double, double> distAngle;
-//     auto minElement = std::min_element(laserScan_.ranges.begin(), laserScan_.ranges.end());
-//     distAngle.first = *minElement;
-//     distAngle.second = std::distance(laserScan_.ranges.begin(), minElement) * laserScan_.angle_increment + laserScan_.angle_min;
-//     return distAngle;
-// }
+std::pair<double, double> LaserProcessing::MinDistAngle() {
+    std::pair<double, double> distAngle;
+    auto minElement = std::min_element(laserScan_.ranges.begin(), laserScan_.ranges.end());
+    distAngle.first = *minElement;
+    distAngle.second = std::distance(laserScan_.ranges.begin(), minElement) * laserScan_.angle_increment + laserScan_.angle_min;
+    return distAngle;
+}
