3876b009ab94510c67ba4d3c3014606c7e9e3caa
stmn8r <ali.sarlak@student.uts.edu.au>
2024-04-08 14:07:51 +1000

    test

--- artbot_code/src/laserprocessing.cpp
+++ artbot_code/src/laserprocessing.cpp
@@ -14,7 +14,18 @@ LaserProcessing::LaserProcessing(sensor_msgs::LaserScan laserScan):
 std::pair<double, double> LaserProcessing::MinDistAngle(){
     std::pair<double, double> distAngle;
     distAngle.first = 0.0;
-    distAngle.second = 0.0;
+    distAngle.second = 1.0;
 
     return distAngle;
 }
+
+// Ali's code
+
+// Getter for distance and angle to the nearest obstacle
+// std::pair<double, double> LaserProcessing::MinDistAngle() {
+//     std::pair<double, double> distAngle;
+//     auto minElement = std::min_element(laserScan_.ranges.begin(), laserScan_.ranges.end());
+//     distAngle.first = *minElement;
+//     distAngle.second = std::distance(laserScan_.ranges.begin(), minElement) * laserScan_.angle_increment + laserScan_.angle_min;
+//     return distAngle;
+// }
